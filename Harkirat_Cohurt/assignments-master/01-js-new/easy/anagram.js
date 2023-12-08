39ace3a7ba5feb8efdc1c54342d7a45563ec4724
Adityavyshnav-2001 <codeadityavyshnav@gmail.com>
2023-12-08 11:10:18 +0530

    update

--- Harkirat_Cohurt/assignments-master/01-js-new/easy/anagram.js
+++ Harkirat_Cohurt/assignments-master/01-js-new/easy/anagram.js
@@ -8,6 +8,7 @@ function isAnagram(str1, str2) {
   if (str1.length !== str2.length) {
     return false;
   }
+  vector<int> adi;
 
   const sortedStr1 = str1.toLowerCase().split("").sort().join("");
   const sortedStr2 = str2.toLowerCase().split("").sort().join("");

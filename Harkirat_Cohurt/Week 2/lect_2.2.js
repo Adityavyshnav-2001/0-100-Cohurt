43d2ee44e0229fae44ccac81c349d5952eb06e2a
Adityavyshnav-2001 <codeadityavyshnav@gmail.com>
2023-12-12 12:09:18 +0530

    update

---
+++ Harkirat_Cohurt/Week 2/lect_2.2.js
@@ -0,0 +1,6 @@
+console.log("hello there");
+function onDone(){
+    console.log("I am Done")
+};
+setTimeout(onDone,10000);
+console.log("i am back");
--- Javascript/adi.js
+++ Javascript/adi.js
@@ -1,2 +1,5 @@
-console.log("hello");
-var a=10;
+let a="Adityavyshnav"
+console.log(a.slice(0,4)) 
+
+let score=100.45688
+console.log(score.toFixed(2))
